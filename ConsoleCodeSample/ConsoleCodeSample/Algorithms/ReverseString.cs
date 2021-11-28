4d19d9d580cec00ffc111aa677d818f7617473d9
Abigail Brackeen <abby@codepoetllc.com>
2021-11-27 19:07:41 -0700

    Added the Uno Reverse functionality w/ validaiton and user input

---
+++ ConsoleCodeSample/ConsoleCodeSample/Algorithms/ReverseString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCodeSample.Algorithms
+{
+    public class ReverseString
+    {
+        public class StringReverseResult
+        {
+            //public string reversedString { get; set; }
+            public bool isSuccessful { get; set; }
+            public string Message { get; set; }
+        }
+
+        //easiest solution
+        public string ReverseStringFunc(string stringInput)
+        {
+            var result = ValidateAndReverse(stringInput);
+            return result.Message;
+                    }
+
+        public StringReverseResult ValidateAndReverse(string stringInput)
+        {
+            var result = new StringReverseResult();
+            var instructions = "Your text must be at least 2 characters long & cannot be just whitespace.";
+
+            if (string.IsNullOrWhiteSpace(stringInput))
+            {
+                result.isSuccessful = false;
+                result.Message = $"You didn't enter anything! {instructions}";
+            }
+            else if (stringInput.Length < 2)
+            {
+                result.isSuccessful = false;
+                result.Message = $"You only put in one number! {instructions}";
+            }
+            else
+            {
+                //convert to array & reverse
+                char[] charArray = stringInput.ToCharArray();
+                Array.Reverse(charArray);
+
+                //convert to string and assign
+               string reversedString = new string(charArray);
+                result.isSuccessful = true;
+                result.Message = $"Here is your reversed text: {reversedString}";
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ ConsoleCodeSample/ConsoleCodeSample/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleCodeSample.Algorithms;
+using System;
+
+namespace ConsoleCodeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Common Item Data ---> Edit to take user input
+            string[] array1 = { "a", "b", "c", "x" };
+            string[] array2 = { "z", "y", "x" };
+            string[] array3 = { "d", "e", "f", "g" };
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Please select the index of your desired method & hit enter.\n" +
+                                "0: Find Common Item - 2 array input\n" +
+                                "1: Uno Reverse - 1 string input");
+
+            string userSelection = Console.ReadLine();
+            //pull this out into another method
+            switch (userSelection)
+            {
+                case "0":
+                    Console.WriteLine("Please enter the two sets of digits you would like to compare & hit enter.\nEx: 12345, 098765");
+                    var userInput2 = Console.ReadLine();
+                    var ci = new CommonItem();
+                    //finish implementing user input
+                    Console.WriteLine($"This should be true: {ci.ContainsCommonItem(array1, array2)}");
+                    Console.WriteLine($"This should be false: {ci.ContainsCommonItem(array1, array3)}");
+                    break;
+
+                case "1":
+                    var reverseString = new ReverseString();
+                    Console.WriteLine("Please enter your desired text & hit enter.");
+                    string userInput1 = Console.ReadLine();
+                    Console.WriteLine(reverseString.ReverseStringFunc(userInput1));
+                    break;
+
+                default:
+                    break;
+            }
+
+            //add way to select again or exit
+            Console.ReadLine();
+        }
+    }
+}
